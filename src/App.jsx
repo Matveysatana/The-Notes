ab1d7f61e40629eaa93a718ed6933846baf4b146
Matvey <example@gmail.com>
2025-05-28 09:55:28 +0300

    First component Button

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import './App.css'
+import Button from './components/Button/Button'
+
+
+function App() {
+
+
+  return (
+    <>
+      <h1 className='titile'>Записная книжка</h1>
+      <Button />
+    </>
+  )
+}
+
+export default App
---
+++ src/components/Button/Button.jsx
@@ -0,0 +1,9 @@
+
+
+const Button = () => {
+    return (
+        <button>Сохранить воспоминание!</button>
+    );
+};
+
+export default Button;
